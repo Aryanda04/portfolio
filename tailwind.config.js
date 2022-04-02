3bee348bf916203e8114d56cfed7f1b8e69e7a72
Aryanda Rizky P <aryandarizky1234@gmail.com>
2022-04-03 05:03:56 +0700

    portfolio work

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0D9488",
+        dark: "#334155",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
